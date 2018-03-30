ac006080380d0dbb160af3e3698c72e0d5d25a5e
Unknown <up201705616@fe.up.pt>
2018-03-31 00:14:19 +0100

    30 03 2018

--- ConsoleApplication4/ConsoleApplication4/ConsoleApplication4.cpp
+++ ConsoleApplication4/ConsoleApplication4/ConsoleApplication4.cpp
@@ -1,11 +1,43 @@
 ﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
 
 using namespace std;
 
-int main(){
+bool verifica(bool flag, string palavra);
 
+int main(){
+	string palavra;
+	vector<string> lista;
+	bool flag;
+	ifstream theFile("29765-8.txt");
+
+	while (theFile >> palavra) {
+		verifica(flag, palavra);
+
+		if (flag) {
+			lista.insert(palavra);
+		}
+	}
+
+	//dps aqui falta o sort
 
     return 0;
 }
 
-//Beto faz tudo pf!
+bool verifica(bool flag, string palavra) {
+	bool flag=true;
+	int i = 0;
+
+	//falta ainda separa as palavras ligadas por ";"
+	
+	for (i; i < palavra.length; i++) {
+		if (isupper (palavra.at(i))) {
+			flag = true;
+		}
+		else { flag = false;}
+	}
+
+	return flag;
+}
