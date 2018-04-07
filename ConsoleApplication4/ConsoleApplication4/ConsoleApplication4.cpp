89f7fc2d353b6e89327e59ea6c5df965b5f0a7bc
Unknown <up201705616@fe.up.pt>
2018-04-07 17:53:40 +0100

    temos um erro que nao sei como resolver quando o programa corre

--- ConsoleApplication4/ConsoleApplication4/ConsoleApplication4.cpp
+++ ConsoleApplication4/ConsoleApplication4/ConsoleApplication4.cpp
@@ -7,35 +7,40 @@ using namespace std;
 
 bool verifica(bool flag, string palavra);
 
-int main(){
+int main() {
 	string palavra;
 	vector<string> lista;
-	bool flag;
+	bool flag = true;
 	ifstream theFile("29765-8.txt");
 
 	while (theFile >> palavra) {
-		verifica(flag, palavra);
+		flag = verifica(flag, palavra);
 
 		if (flag) {
-			lista.insert(palavra);
+			lista.push_back(palavra);
+			//lista.insert(palavra);
 		}
 	}
 
+	for (int v = 0; v < lista.size(); v++) {
+		cout << lista[v] << endl;
+	}
 	//dps aqui falta o sort
 
-    return 0;
+	return 0;
 }
 
 bool verifica(bool flag, string palavra) {
-	bool flag=true;
+	flag = true;
 	int i = 0;
 
-	//falta ainda separa as palavras ligadas por ";"
-	
-	for (i; i < palavra.length; i++) {
-		if (isupper (palavra.at(i))) {
+	for (i; i < palavra.size(); i++) {
+		if (isupper(palavra.at(i))) {
 			flag = true;
 		}
+		else if (((palavra.at(i)) == ' ') || (palavra.at(i)) == ';' || (palavra.at(i)) == '-' || (palavra.at(i)) == 39){
+		flag = true;
+}
 		else { flag = false;}
 	}
 
